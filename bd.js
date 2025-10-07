e17bc4f7dbbf5eb0bd78d7cbe0b631a61ffb5698
helejalde <helejaldec@gmail.com>
2025-10-06 22:23:44 -0500

    Arreglo de bd.js

--- bd.js
+++ bd.js
@@ -1,20 +1,15 @@
 const { Pool } = require('pg');
 
-// Configuración de la base de datos
 const pool = new Pool({
-    user: 'postgres',          // Usuario de la base de datos
-    host: 'localhost',           // Dirección del servidor (cambiar si es remoto)
-    database: 'UERB', // Nombre de la base de datos
-    password: 'Admin123',   // Contraseña del usuario
-    port: 5432,                  // Puerto de conexión (por defecto es 5432)
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false
 });
 
-// Verificar la conexión
-pool.connect()
-    .then(client => {
-        console.log("Conexión exitosa a la base de datos");
-        client.release(); // Liberar el cliente
-    })
-    .catch(err => console.error('Error de conexión a la base de datos', err.stack));
+// Opcional: escuchar errores de cliente inactivo
+pool.on('error', err => {
+  console.error('Postgres idle client error', err);
+});
 
 module.exports = pool;
