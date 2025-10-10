2c6bff1875a27b819ef5e93bf16b139b309cbe2d
helejalde <helejaldec@gmail.com>
2025-10-10 09:32:04 -0500

    solucion de enlace al que debe llamar la conexion de inicio de secion, ya no local host

--- bd.js
+++ bd.js
@@ -1,20 +1,17 @@
 const { Pool } = require('pg');
 
-// Configuración de la base de datos
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
-    user: 'postgres',          // Usuario de la base de datos
-    host: 'localhost',           // Dirección del servidor (cambiar si es remoto)
-    database: 'UERB', // Nombre de la base de datos
-    password: 'Admin123',   // Contraseña del usuario
-    port: 5432,                  // Puerto de conexión (por defecto es 5432)
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
-// Verificar la conexión
 pool.connect()
-    .then(client => {
-        console.log("Conexión exitosa a la base de datos");
-        client.release(); // Liberar el cliente
-    })
-    .catch(err => console.error('Error de conexión a la base de datos', err.stack));
+  .then(client => {
+    console.log('Conexión exitosa a la base de datos');
+    client.release();
+  })
+  .catch(err => console.error('Error de conexión a la base de datos', err.stack));
 
 module.exports = pool;
